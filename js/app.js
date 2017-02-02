309ffbdeeee4802604f3b7f39a83a481d560ab74
Wojciech Wiński <wojciech.winski@gmail.com>
2017-02-02 22:51:20 +0100

    slider

--- js/app.js
+++ js/app.js
@@ -31,18 +31,70 @@ $('.galleryItem').on('mouseleave',function(){
   console.log('click');
   $(this).children().show();
 });
-var sliderPosition = 0;
+
+var sliderPosition = 1;
 $('.leftSliderButton').on('click',function(){
   console.log('left');
-  sliderPosition += 1064;
-  if (sliderPosition > 0) sliderPosition = -2128;
-  $('.frameSlider').css('left',sliderPosition+'px');
+
+  // if (sliderPosition > 0) sliderPosition = -2128;
+  // $('.frameSlider').css('left',sliderPosition+'px');
+  if (sliderPosition == 1) {
+    $( ".frameSlider" ).animate({
+      left: "+=1064"
+    }, 1000, function() {
+      // Animation complete.
+
+    });
+    $( ".frameSlider" ).animate({
+      left: "-3192"
+    }, 0, function() {
+      // Animation complete.
+    });
+    sliderPosition = 4;
+  } else {
+    sliderPosition -= 1;
+    $( ".frameSlider" ).animate({
+      left: "+=1064"
+    }, 1000, function() {
+      // Animation complete.
+    });
+  };
+  console.log('left',sliderPosition);
 });
+
 $('.rightSliderButton').on('click',function(){
   console.log('right');
-  sliderPosition -= 1064;
-  if (sliderPosition < -2128) sliderPosition = 0;
-  $('.frameSlider').css('left',sliderPosition+'px');
+  sliderPosition += 1;
+  // if (sliderPosition < -2128) sliderPosition = 0;
+  // $('.frameSlider').css('left',sliderPosition+'px');
+  if (sliderPosition == 4) {
+    $( ".frameSlider" ).animate({
+      left: "-=1064"
+    }, 1000, function() {
+      // Animation complete.
+
+    });
+    // $('.frameSlider').css('left',-1064*sliderPosition +'px');
+    $( ".frameSlider" ).animate({
+      left: "-1064"
+    }, 0, function() {
+      // Animation complete.
+    });
+    console.log($('.frameSlider'))
+    sliderPosition = 1;
+  } else {
+    $( ".frameSlider" ).animate({
+      left: "-=1064"
+    }, 1000, function() {
+      // Animation complete.
+    });
+  };
+  // $( ".frameSlider" ).animate({
+  //   left: "-=1064"
+  // }, 1000, function() {
+  //   // Animation complete.
+  // });
+  console.log('right',sliderPosition);
 });
 
 
