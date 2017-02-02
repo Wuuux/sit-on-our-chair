7e1bc104a9955f9b2bfe6121edc61ec8b6899305
Wojciech Wiński <wojciech.winski@gmail.com>
2017-02-02 18:15:15 +0100

    slider simple

--- js/app.js
+++ js/app.js
@@ -1,35 +1,49 @@
-document.addEventListener("DOMContentLoaded", function(){
+$(function() {
 
 
-  // var buttons = document.getElementsByTagName('button');
-  // var divId = document.getElementById('id1');
-  //
-  // for(var i=0;i<buttons.length;i++){
-  //   console.log(buttons[i]);
-  //   buttons[i].addEventListener('click',klik)
-  // }
-  var submenuHidden = true;
-  function showSubmenu(){
-    // divId.innerHTML = this.dataset.text;
-    console.log('klik');
-    var submenu = document.getElementById('submenu');
-    if (submenuHidden == true) {
-      submenu.style.visibility = 'visible';
-      submenuHidden = false;
-    } else {
-      submenu.style.visibility = 'hidden';
-      submenuHidden = true;
-    }
-  };
-  function slideSubmenu(){
-    // divId.innerHTML = this.dataset.text;
-    console.log('click1');
-  };
+//   var submenuHidden = true;
+//   function showSubmenu(){
+//     // divId.innerHTML = this.dataset.text;
+//     console.log('klik');
+//     var submenu = document.getElementById('submenu');
+//     if (submenuHidden == true) {
+//       submenu.style.visibility = 'visible';
+//       submenuHidden = false;
+//     } else {
+//       submenu.style.visibility = 'hidden';
+//       submenuHidden = true;
+//     }
+//   };
+//   function slideSubmenu(){
+//     // divId.innerHTML = this.dataset.text;
+//     console.log('click1');
+//   };
+//
+// var navLink= document.getElementsByTagName('li');
+// // navLink[0].addEventListener('onmouseenter',klik1);
+// navLink[0].addEventListener('click',slideSubmenu);
 
-var navLink= document.getElementsByTagName('li');
-navLink[0].addEventListener('onmouseenter',klik1);
-navLink[0].addEventListener('click',slideSubmenu);
-
+$('.galleryItem').on('mouseenter',function(){
+  console.log('click');
+  $(this).children().hide();
+});
+$('.galleryItem').on('mouseleave',function(){
+  console.log('click');
+  $(this).children().show();
+});
+var sliderPosition = 0;
+$('.leftSliderButton').on('click',function(){
+  console.log('left');
+  sliderPosition += 1064;
+  if (sliderPosition > 0) sliderPosition = -2128;
+  $('.frameSlider').css('left',sliderPosition+'px');
+});
+$('.rightSliderButton').on('click',function(){
+  console.log('right');
+  sliderPosition -= 1064;
+  if (sliderPosition < -2128) sliderPosition = 0;
+  $('.frameSlider').css('left',sliderPosition+'px');
+});
 
 
 });
